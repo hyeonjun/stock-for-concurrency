a55b8185a784459ab749e92d9a300a41042f6397
hjju <hjju@fin2b.com>
2024-02-24 21:31:14 +0900

    stock entity, repository, service 추가

---
+++ stock/src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,26 @@
+package com.example.stock.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Stock {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private Long productId;
+  private Long quantity;
+
+  public Stock(Long productId, Long quantity) {
+    this.productId = productId;
+    this.quantity = quantity;
+  }
+}
