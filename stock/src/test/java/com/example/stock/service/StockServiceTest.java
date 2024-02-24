7eaeaa0c11d1bdf795bbfff458eb101be9e99faa
hjju <hjju@fin2b.com>
2024-02-24 23:24:01 +0900

    동시에 재고감소 100개 요청 테스트

--- stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -1,19 +1,24 @@
 package com.example.stock.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.annotation.Rollback;
 
 @SpringBootTest
-@Transactional
+@Rollback(value = false)
 public class StockServiceTest {
 
   @Autowired
@@ -21,21 +26,58 @@ public class StockServiceTest {
   @Autowired
   private StockRepository stockRepository;
 
-  private Logger log = LoggerFactory.getLogger(StockServiceTest.class);
+  private final Logger log = LoggerFactory.getLogger(StockServiceTest.class);
+  private final Stock initStock = new Stock(1L, 100L);
 
   @BeforeEach
   public void before() {
-    stockRepository.save(new Stock(1L, 100L));
+    stockRepository.save(initStock);
+  }
+
+  @AfterEach
+  public void after() {
+    stockRepository.delete(initStock);
   }
 
   @Test
   public void 재고감소() {
-    stockService.decrease(1L, 1L);
+    log.info("stock id: {}, quantity: {}", initStock.getId(), initStock.getQuantity());
+    stockService.decrease(initStock.getId(), 1L);
 
-    Stock stock = stockService.getStock(1L);
-
+    Stock stock = stockService.getStock(initStock.getId());
     log.info("{}", stock);
 
     assertEquals(99, stock.getQuantity());
+  }
+
+  @Test
+  public void 동시에_재고감소_100개_요청() throws InterruptedException {
+    log.info("stock id: {}, quantity: {}", initStock.getId(), initStock.getQuantity());
+
+    int threadCount = 100;
+    // 멀티스레드 이용 ExecutorService: 비동기를 단순하게 처리할 수 있도록 해주는 Java API
+    ExecutorService executorService = Executors.newFixedThreadPool(32);
+    // 다른 스레드에서 수행이 완료될 때까지 대기할 수 있도록 도와주는 API - 요청이 끝날 때까지 기다림
+    CountDownLatch latch = new CountDownLatch(threadCount);
+
+    for (int i=0; i<threadCount; i++) {
+      executorService.submit(() -> {
+        try {
+          stockService.decrease(initStock.getId(), 1L);
+        } catch (Exception e) {
+          log.error(e.getMessage()); // 재고를 찾지 못하는 RuntimeException 발생
+        } finally {
+          latch.countDown();
+        }
+      });
+    }
+
+    latch.await();
+
+    Stock stock = stockService.getStock(initStock.getId());
+    log.info("stock: {}", stock);
+
+    // 100 - (1 * 100) = 0
+    assertNotEquals(0, stock.getQuantity());
   }
 }
