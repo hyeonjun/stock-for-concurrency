f24dea512feca078a032394b1679c636c2d57baa
hjju <hjju@fin2b.com>
2024-02-25 00:16:15 +0900

    Synchronized 키워드를 사용한 동시 재고감소 100개 요청 로직 추가 및 테스트

--- stock/src/main/java/com/example/stock/service/StockService.java
+++ stock/src/main/java/com/example/stock/service/StockService.java
@@ -8,11 +8,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class StockService {
 
   private final StockRepository stockRepository;
 
+  @Transactional(readOnly = true)
   public Stock getStock(Long id) {
     return stockRepository.findById(id)
       .orElseThrow(() -> new RuntimeException("재고가 없습니다."));
@@ -20,6 +20,24 @@ public class StockService {
 
   @Transactional
   public void decrease(Long id, Long quantity) {
+    // Stock 조회
+    Stock stock = stockRepository.findById(id)
+      .orElseThrow(() -> new RuntimeException("재고가 없습니다."));
+
+    // 재고 감소
+    stock.decrease(quantity);
+
+    // 갱신된 값을 저장
+    stockRepository.save(stock);
+  }
+
+  /**
+   * synchronized 키워드를 메소드 선언부에 붙여주면 한 개의 스레드만 접근가능하게 한다.
+   * synchronized 사용시 @Transactional과 동시에 사용하면 안된다.
+   * Synchronized를 사용하는 이유는 해당 메소드를 한 쓰레드에서만 돌리기 위해서다.
+   * 하지만, 트랜잭션이 같이 정의가 되어있다면 첫 번째 쓰레드가 끝나기 전 두 번째 쓰레드가 발동할 수도 있다.
+   */
+  public synchronized void decreaseSynchronized(Long id, Long quantity) {
     // Stock 조회
     Stock stock = stockRepository.findById(id)
       .orElseThrow(() -> new RuntimeException("재고가 없습니다."));
