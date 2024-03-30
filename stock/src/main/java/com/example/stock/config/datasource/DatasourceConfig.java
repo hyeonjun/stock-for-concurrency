fc957fd38c9e2fc702d31f27d70a0f15d0a9fc7b
hjju <hjju@fin2b.com>
2024-03-31 02:41:58 +0900

    -

--- stock/src/main/java/com/example/stock/config/datasource/DatasourceConfig.java
+++ stock/src/main/java/com/example/stock/config/datasource/DatasourceConfig.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
